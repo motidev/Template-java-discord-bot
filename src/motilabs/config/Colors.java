e7fe615692468775f3ed2caba361da54a516246e
motidev <angelnabro@gmail.com>
2023-02-19 13:05:39 +0100

    colors class

---
+++ src/motilabs/config/Colors.java
@@ -0,0 +1,7 @@
+package motilabs.config;
+
+public class Colors {
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+}
